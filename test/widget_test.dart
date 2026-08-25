60bb792d5cdfc089567efae1b44d01dbe6ab640d
Rayat23 <69200637+Rayat23@users.noreply.github.com>
2026-08-24 18:54:19 -0700

    Test movie runtime parsing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@ import 'package:movie_tracker/models/movie.dart';
 import 'package:movie_tracker/models/tv_show.dart';
 
 void main() {
-  test('Movie parses TMDB data', () {
+  test('Movie parses TMDB data including runtime', () {
     final movie = Movie.fromJson({
       'id': 1,
       'title': 'Example Movie',
@@ -13,11 +13,14 @@ void main() {
       'vote_average': 8.2,
       'release_date': '2026-01-01',
       'overview': 'Example overview',
+      'runtime': 149,
     });
 
     expect(movie.id, 1);
     expect(movie.title, 'Example Movie');
     expect(movie.rating, 8.2);
+    expect(movie.runtimeMinutes, 149);
+    expect(movie.toJson()['runtime'], 149);
   });
 
   test('TV show and episode parse TMDB data', () {
