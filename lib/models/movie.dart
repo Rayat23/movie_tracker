9923ba3541549e855757a5ad3316f8fb85c9e504
Rayat23 <69200637+Rayat23@users.noreply.github.com>
2026-08-24 18:52:17 -0700

    Add movie runtime metadata

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -5,6 +5,7 @@ class Movie {
   final double rating;
   final String releaseDate;
   final String overview;
+  final int runtimeMinutes;
 
   const Movie({
     required this.id,
@@ -13,31 +14,27 @@ class Movie {
     required this.rating,
     required this.releaseDate,
     required this.overview,
+    this.runtimeMinutes = 0,
   });
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
       id: json['id'] as int? ?? 0,
-
       title:
           json['title'] as String? ??
           json['name'] as String? ??
           'Unknown title',
-
       posterPath: json['poster_path'] as String? ?? '',
-
       rating: (json['vote_average'] as num?)?.toDouble() ?? 0.0,
-
       releaseDate:
           json['release_date'] as String? ??
           json['first_air_date'] as String? ??
           '',
-
       overview: json['overview'] as String? ?? '',
+      runtimeMinutes: json['runtime'] as int? ?? 0,
     );
   }
 
-  // Converts Movie into a map so it can be saved.
   Map<String, dynamic> toJson() {
     return {
       'id': id,
@@ -46,6 +43,7 @@ class Movie {
       'vote_average': rating,
       'release_date': releaseDate,
       'overview': overview,
+      'runtime': runtimeMinutes,
     };
   }
 
